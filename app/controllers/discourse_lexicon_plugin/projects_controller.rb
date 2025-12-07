0ea7a44f06a3f339100c53708556e11b7893e52d
EmilFrom <emilfrom@gmail.com>
2025-12-07 14:06:08 +0100

    Update projects_controller.rb

--- app/controllers/discourse_lexicon_plugin/projects_controller.rb
+++ app/controllers/discourse_lexicon_plugin/projects_controller.rb
@@ -3,11 +3,15 @@ module DiscourseLexiconPlugin
     skip_before_action :verify_authenticity_token
     
     def create
+      Rails.logger.warn("[Lexicon] ProjectsController#create called")
       require_dependency 'category_creator'
+      Rails.logger.warn("[Lexicon] Dependency loaded")
+
       params.require(:name)
+      Rails.logger.warn("[Lexicon] Params: #{params.inspect}")
+      Rails.logger.warn("[Lexicon] Current User: #{current_user&.username}")
       
       # 1. Create the Category
-      # We use the system user to bypass permission checks, but we set the creator as the current user
       category_args = {
         name: params[:name],
         description: params[:description],
@@ -16,30 +20,34 @@ module DiscourseLexiconPlugin
         user: current_user
       }
 
-      # Use CategoryCreator to handle all the heavy lifting (slugs, validation, etc.)
+      Rails.logger.warn("[Lexicon] Initializing CategoryCreator")
       creator = ::CategoryCreator.new(Discourse.system_user, category_args)
+      
+      Rails.logger.warn("[Lexicon] Creating category...")
       category = creator.create
+      Rails.logger.warn("[Lexicon] Category created: #{category&.id}")
 
       if creator.errors.present?
+        Rails.logger.warn("[Lexicon] Creator errors: #{creator.errors.full_messages}")
         return render_json_error(creator.errors.full_messages.join(", "))
       end
 
-      # 2. Set Permissions (Everyone can See/Reply/Create)
-      # 0 = Everyone
-      # 1 = Full (See, Create, Reply)
+      # 2. Set Permissions
+      Rails.logger.warn("[Lexicon] Setting permissions...")
       category.set_permissions({ 0 => 1 })
       category.save!
 
-      # 3. (Optional) Create Chat Channel
+      # 3. Create Chat Channel
       chat_channel = nil
       if params[:create_chat] == 'true' && defined?(::Chat::Channel)
-        # Create a category channel
+        Rails.logger.warn("[Lexicon] Creating chat channel...")
         chat_channel = ::Chat::Channel.create!(
           chatable: category,
           chatable_type: 'Category',
           name: "General",
           user: current_user
         )
+        Rails.logger.warn("[Lexicon] Chat channel created: #{chat_channel&.id}")
       end
 
       render json: {
@@ -52,6 +60,8 @@ module DiscourseLexiconPlugin
         chat_channel_id: chat_channel&.id
       }
     rescue => e
+      Rails.logger.error("[Lexicon] ERROR: #{e.message}")
+      Rails.logger.error(e.backtrace.join("\n"))
       render_json_error(e.message)
     end
   end
