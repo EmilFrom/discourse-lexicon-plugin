08b96cab4a8030a924730f29cec73a9ac3a17bf3
EmilFrom <emilfrom@gmail.com>
2025-12-10 15:12:15 +0100

    Added groups to be able to create projects

--- app/controllers/discourse_lexicon_plugin/projects_controller.rb
+++ app/controllers/discourse_lexicon_plugin/projects_controller.rb
@@ -11,18 +11,34 @@ module DiscourseLexiconPlugin
       validation_error = validate_params
       return render_json_error(validation_error, status: 400) if validation_error
 
+      norm = normalized_params
+
       guardian = Guardian.new(current_user)
-      unless guardian.can_create_category?
+      allowed_group = Group.find_by(id: SiteSetting.lexicon_projects_allowed_group)
+      allowed_by_group = allowed_group && current_user.groups.exists?(id: allowed_group.id)
+      unless guardian.can_create_category? || allowed_by_group
         return render_json_error("You are not allowed to create categories", status: 403)
       end
 
-      category = build_category
+      category = nil
+      chat_channel = nil
+      chat_warning = nil
 
-      Rails.logger.warn("[Lexicon] Setting permissions...")
-      category.set_permissions({ 0 => 1 })
-      category.save!
+      Category.transaction do
+        category = build_category(norm)
 
-      chat_channel = maybe_create_chat_channel(category)
+        Rails.logger.warn("[Lexicon] Setting permissions...")
+        category.set_permissions({ 0 => 1 })
+        category.save!
+      end
+
+      begin
+        chat_channel = maybe_create_chat_channel(category, norm[:create_chat])
+      rescue => chat_error
+        Rails.logger.warn("[Lexicon] Chat channel creation failed: #{chat_error.message}")
+        Rails.logger.warn(chat_error.backtrace.join("\n"))
+        chat_warning = "Category created but chat channel could not be created"
+      end
 
       render json: {
         category: {
@@ -31,7 +47,8 @@ module DiscourseLexiconPlugin
           slug: category.slug,
           url: category.url
         },
-        chat_channel_id: chat_channel&.id
+        chat_channel_id: chat_channel&.id,
+        warning: chat_warning
       }
     rescue => e
       Rails.logger.error("[Lexicon] ERROR: #{e.message}")
@@ -45,28 +62,46 @@ module DiscourseLexiconPlugin
       name = params[:name].to_s.strip
       return "Name is required" if name.blank?
 
-      if params[:color].present?
-        return "Color must be a 6 character hex value" unless params[:color].match?(/\A[0-9a-fA-F]{6}\z/)
+      if name.length > 50
+        return "Name is too long (max 50 characters)"
       end
 
-      if params[:description].present? && params[:description].to_s.length > 1000
+      desc = params[:description].to_s.strip
+      if desc.length > 1000
         return "Description is too long (max 1000 characters)"
       end
 
       nil
     end
 
-    def build_category
+    def normalized_params
+      name = params[:name].to_s.strip
+      description = params[:description].to_s.strip
+      description = nil if description.blank?
+
+      color = params[:color].to_s.strip.downcase
+      color = "0088cc" unless color.match?(/\A[0-9a-f]{6}\z/)
+
+      {
+        name: name,
+        description: description,
+        color: color,
+        text_color: "ffffff",
+        create_chat: ActiveModel::Type::Boolean.new.cast(params[:create_chat])
+      }
+    end
+
+    def build_category(norm)
       category = nil
 
       begin
         if defined?(::CategoryCreator)
           Rails.logger.warn("[Lexicon] Using CategoryCreator")
           category_args = {
-            name: params[:name],
-            description: params[:description],
-            color: params[:color] || "0088CC",
-            text_color: "FFFFFF",
+            name: norm[:name],
+            description: norm[:description],
+            color: norm[:color],
+            text_color: norm[:text_color],
             user: current_user
           }
           creator = ::CategoryCreator.new(current_user, category_args)
@@ -87,25 +122,24 @@ module DiscourseLexiconPlugin
     end
 
     def build_category_fallback
-      Category.transaction do
-        category = Category.create!(
-          name: params[:name],
-          user: current_user,
-          color: params[:color] || "0088CC",
-          text_color: "FFFFFF"
-        )
+      norm = normalized_params
 
-        if params[:description].present?
-          category.update!(description: params[:description])
-        end
+      category = Category.create!(
+        name: norm[:name],
+        user: current_user,
+        color: norm[:color],
+        text_color: norm[:text_color]
+      )
 
-        category
+      if norm[:description].present?
+        category.update!(description: norm[:description])
       end
+
+      category
     end
 
-    def maybe_create_chat_channel(category)
-      create_chat = ActiveModel::Type::Boolean.new.cast(params[:create_chat])
-      return nil unless create_chat
+    def maybe_create_chat_channel(category, create_chat_flag)
+      return nil unless create_chat_flag
       return nil unless defined?(::Chat::Channel)
 
       Rails.logger.warn("[Lexicon] Creating chat channel...")
